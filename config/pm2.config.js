2d9ac889713301e3670598553ac3d8ff4a9a9e59
Max <shpax1@gmail.com>
2018-04-18 22:23:05 +0300

    pm2 config limitations

--- config/pm2.config.js
+++ config/pm2.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   apps: [
     {
-      "script": "./app.js",
-      "name": "iot-ws-registrator",
-      "watch": ["config"]
+      'script': './app.js',
+      'name': 'iot-ws-registrator',
+      'watch': ['config'],
+      'watch -n 5 free -m': '100M'
     }
   ]
 };
